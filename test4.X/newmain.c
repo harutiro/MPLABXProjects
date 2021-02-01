4b68103c02bd192781d6a5499aaa22c677bec85a
harut <harutiro2027@outlook.jp>
2021-02-02 01:02:03 +0900

    testCommit:

---
+++ test4.X/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: harut
+ *
+ * Created on 2021/02/02, 0:59
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
