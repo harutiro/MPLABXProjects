4eb6e0001b18c8b708de96e7f5800a196e0c8f11
harut <harutiro2027@outlook.jp>
2021-03-04 16:31:57 +0900

    add: H27　start

---
+++ SKT_h27_vor1.X/Q1.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Q1.c
+ * Author: harut
+ *
+ * Created on 2021/03/04, 16:31
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
